7f1e3ca7f3452226056b635f952d7f90ee15696a
Denis <56925572+TayLight@users.noreply.github.com>
2020-10-11 11:02:50 +0400

    Поиск аккаунта клиента

--- src/main/java/com/securebank/securebank/repository/ClientRepository.java
+++ src/main/java/com/securebank/securebank/repository/ClientRepository.java
@@ -1,7 +1,10 @@
 package com.securebank.securebank.repository;
 
+import com.securebank.securebank.entity.Account;
 import com.securebank.securebank.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+    public Account findAccountByIdClient(int idClient);
 }
